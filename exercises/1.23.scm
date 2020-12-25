bdc54963056f4ca1b8fa124dd96206b1c7befe97
vladi99 <vlpetrovic99@gmail.com>
2020-12-26 01:04:11 +0200

    1.24.scm

--- exercises/1.23.scm
+++ exercises/1.23.scm
@@ -22,27 +22,33 @@
   (= n (smallest-divisor n))) 
   
 (define (timed-prime-test n) 
-  (start-prime-test n (runtime))) 
-  
+  (start-prime-test n (runtime)))
+
 (define (start-prime-test n start-time) 
   (if (prime? n) 
-      (report-prime n (- (runtime) start-time)))) 
-  
-(define (report-prime n elapsed-time) 
-  (newline) 
-  (display n) 
-  (display " *** ") 
-  (display elapsed-time))
+      (report-prime n (- (runtime) start-time)) 
+      #f))
 
-(timed-prime-test 1000000007) 
-(timed-prime-test 1000000009) 
-(timed-prime-test 1000000021) 
-(timed-prime-test 10000000019) 
-(timed-prime-test 10000000033) 
-(timed-prime-test 10000000061) 
-(timed-prime-test 100000000003) 
-(timed-prime-test 100000000019) 
-(timed-prime-test 100000000057) 
-(timed-prime-test 1000000000039) 
-(timed-prime-test 1000000000061) 
-(timed-prime-test 1000000000063)
+(define (report-prime n elapsed-time) 
+  (display n) 
+  (display "***") 
+  (display elapsed-time) 
+  (newline)) 
+
+(define (search-for-primes n counter) 
+  (if (even? n) 
+      (s-f-p (+ n 1) counter) 
+      (s-f-p n counter)))
+
+(define (s-f-p n counter) 
+  (if (> counter 0) 
+      (if (timed-prime-test n) 
+          (s-f-p (+ n 2) (- counter 1)) 
+          (s-f-p (+ n 2) counter)) 
+      "COMPUTATION COMPLETE"))
+  
+
+(search-for-primes 1000000000000 3) 
+(search-for-primes 10000000000000 3)
+(search-for-primes 100000000000000 3)
+
--- exercises/1.24.scm
+++ exercises/1.24.scm
@@ -1,27 +1,35 @@
 #lang sicp
 
+(#%require (lib "27.ss" "srfi"))
+
 (define (square x) (* x x)) 
   
-(define (smallest-divisor n) 
-  (find-divisor n 2)) 
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
   
-(define (find-divisor n test-divisor) 
-  (cond ((> (square test-divisor) n) n) 
-        ((divides? test-divisor n) test-divisor) 
-        (else (find-divisor n (+ test-divisor 1))))) 
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random-integer (- n 1))))) 
   
-(define (divides? a b) 
-  (= (remainder b a) 0)) 
-  
-(define (prime? n) 
-  (= n (smallest-divisor n))) 
+(define (fast-prime? n times) 
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false))) 
 
-;test
+; test
+
 (define (timed-prime-test n) 
   (start-prime-test n (runtime)))
 
 (define (start-prime-test n start-time) 
-  (if (prime? n) 
+  (if (fast-prime? n 100) 
       (report-prime n (- (runtime) start-time)) 
       #f))
 
@@ -46,4 +54,5 @@
 
 (search-for-primes 1000000000000 3) 
 (search-for-primes 10000000000000 3)
-(search-for-primes 100000000000000 3)
+(search-for-primes 100000000000000 3) 
+
